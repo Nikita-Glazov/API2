a930b1dd9b2aa96d29e62a6c275c77d40a3ad229
Glazov Nikita <niglazov@gmail.com>
2023-03-28 20:01:23 +0300

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        Post post = new Post();
+        post.name = "Иван";
+        post.passport = "4444 № 44444444";
+        post.patronymic = "Иванович";
+        post.surname = "Иванов";
+        post.phone = "+7 (999)-999-99-99";
+        post.subscription = true;
+        post.birthday.day = 14;
+        post.birthday.month = 10;
+        post.birthday.year = 1985;
+    }
+}
+
